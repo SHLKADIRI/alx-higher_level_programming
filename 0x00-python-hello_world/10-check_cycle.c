286585e94844519319ee03b3fd29bddc2730c0d9
SHLKADIRI <souhailkadiri95@gmail.com>
2023-06-06 01:56:34 +0100

    hellooooo

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check linked list if they contain a cyclee
+ * @list: chuf link
+ * Return: 1ida wla 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
