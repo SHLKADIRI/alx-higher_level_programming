580b1ebe1e55ebe547216aa1a5d14c036df3192c
SHLKADIRI <souhailkadiri95@gmail.com>
2023-06-08 15:39:48 +0100

    3rd tetst task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    total = 0
+    s = 0
     for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+        s += int(sys.argv[i + 1])
+    print("{}".format(s))
